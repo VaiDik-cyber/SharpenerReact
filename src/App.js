36dfea03f4b5dff611fbc1a69b06b820ea1613a9
VaiDik-cyber <patelvaidik56@gmail.com>
2023-06-18 18:20:51 +0530

    React task 7

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ExpenseItems from "./components/ExpenseItem.js";
 function App() {
   return (
     <div>
-      <h2>Let's get start with React</h2>
+      <h3>Let's get start with React</h3>
       <ExpenseItems></ExpenseItems>
     </div>
   );
