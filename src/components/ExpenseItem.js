40391906356a7c6d4bc4bcbdfce7e3cc3865ecc3
VaiDik-cyber <patelvaidik56@gmail.com>
2023-06-20 18:41:36 +0530

    module 1 Task 8

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
-function ExpenseItems() {
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 4, 17);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure = "Pune";
   return (
-    <div>
-      <h2>ExpenseItems</h2>
-      <p>Food Rs.10</p>
-      <p>Petrol Rs.100</p>
-      <p>Movies Rs.200</p>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
     </div>
   );
 }
-export default ExpenseItems;
+
+export default ExpenseItem;
