a2e624ba6af183a918e6fe2e177ff2077c69d5ad
VaiDik-cyber <patelvaidik56@gmail.com>
2023-06-20 19:19:26 +0530

    module 1 task 9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import "./App.css";
-import ExpenseItems from "./components/ExpenseItem.js";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      LocationOfExpenditure: "Pune",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      LocationOfExpenditure: "Nasik",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      LocationOfExpenditure: "Mumbai",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      LocationOfExpenditure: "Dhule",
+    },
+  ];
   return (
     <div>
-      <h3>Let's get start with React</h3>
-      <ExpenseItems></ExpenseItems>
+      <h2>Let's get started!</h2>
+
+      {expenses.map((expense) => (
+        <ExpenseItem item={expense}></ExpenseItem>
+      ))}
     </div>
   );
 }
-
 export default App;
