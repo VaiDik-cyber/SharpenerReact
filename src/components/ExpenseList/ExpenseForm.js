31a42006094abb43cfc873e2e45b272739f35352
VaiDik-cyber <patelvaidik56@gmail.com>
2023-07-04 14:51:04 +0530

    module 2 task 6 expenseData object created

--- src/components/ExpenseList/ExpenseForm.js
+++ src/components/ExpenseList/ExpenseForm.js
@@ -14,8 +14,19 @@ export const ExpenseForm = () => {
   const dateChangedHandler = (e) => {
     setEnteredDate(e.target.value);
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
